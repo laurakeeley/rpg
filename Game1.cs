4353b24daca0eb4b9896c8fd1bcec064ea9af838
Laura Keeley <laura@Lauras-MacBook-Pro.local>
2025-10-13 17:03:35 -0500

    adds player and animations

---
+++ Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using Comora;
+
+namespace rpg;
+
+    enum Dir { Down, Up, Left, Right }
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    
+    Texture2D playerSprite;
+    Texture2D walkDown;
+    Texture2D walkUp;
+    Texture2D walkRight;
+    Texture2D walkLeft;
+    
+    Texture2D background;
+    Texture2D ball;
+    Texture2D skull;
+    
+    Player player = new Player();
+    
+    Camera camera;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _graphics.PreferredBackBufferWidth = 1280;
+        _graphics.PreferredBackBufferHeight = 720;
+        _graphics.ApplyChanges();
+        
+        this.camera = new Camera(_graphics.GraphicsDevice);
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        playerSprite = Content.Load<Texture2D>("Player/player");
+        walkDown = Content.Load<Texture2D>("Player/walkDown");
+        walkUp = Content.Load<Texture2D>("Player/walkUp");
+        walkLeft = Content.Load<Texture2D>("Player/walkLeft");
+        walkRight = Content.Load<Texture2D>("Player/walkRight");
+        
+        background = Content.Load<Texture2D>("background");
+        ball = Content.Load<Texture2D>("ball");
+        skull = Content.Load<Texture2D>("skull");
+
+        player.animations[0] = new SpriteAnimation(walkDown, 4, 8);
+        player.animations[1] = new SpriteAnimation(walkUp, 4, 8);
+        player.animations[2] = new SpriteAnimation(walkLeft, 4, 8);
+        player.animations[3] = new SpriteAnimation(walkRight, 4, 8);
+
+        player.anim = player.animations[0];
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        player.Update(gameTime);
+        
+        this.camera.Position = player.Position;
+        this.camera.Update(gameTime);
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin(this.camera);
+        _spriteBatch.Draw(background, new Vector2(-500, -500), Color.White);
+        // _spriteBatch.Draw(playerSprite, player.Position, Color.White);
+        player.anim.Draw(_spriteBatch);
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,111 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace rpg;
+
+public class Player
+{
+    private Vector2 position = new Vector2(500, 300);
+    private int speed = 300;
+    private Dir direction = Dir.Down;
+    private bool isMoving = false;
+
+    public SpriteAnimation anim;
+
+    public SpriteAnimation[] animations = new SpriteAnimation[4];
+
+    public Vector2 Position
+    {
+        get
+        {
+            return position;
+        }
+    }
+
+    public void setX(float newX)
+    {
+        position.X = newX;
+    }
+
+    public void setY(float newY)
+    {
+        position.Y = newY;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        KeyboardState kState = Keyboard.GetState();
+        float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        isMoving = false;
+
+        if (kState.IsKeyDown(Keys.Right))
+        {
+            direction = Dir.Right;
+            isMoving = true;
+        }
+        if (kState.IsKeyDown(Keys.Left))
+        {
+            direction = Dir.Left;
+            isMoving = true;
+        }
+        if (kState.IsKeyDown(Keys.Up))
+        {
+            direction = Dir.Up;
+            isMoving = true;
+        }
+        if (kState.IsKeyDown(Keys.Down))
+        {
+            direction = Dir.Down;
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            switch (direction)
+            {
+                case Dir.Right:
+                    position.X += speed * dt;
+                    break;
+                case Dir.Left:
+                    position.X -= speed * dt;
+                    break;
+                case Dir.Up:
+                    position.Y -= speed * dt;
+                    break;
+                case Dir.Down:
+                    position.Y += speed * dt;
+                    break;
+            }
+        }
+
+        // switch (direction)
+        // {
+        //     case Dir.Down:
+        //         anim = animations[0];
+        //         break;
+        //     case Dir.Up:
+        //         anim = animations[1];
+        //         break;
+        //     case Dir.Left:
+        //         anim = animations[2];
+        //         break;
+        //     case Dir.Right:
+        //         anim = animations[3];
+        //         break;
+        // }
+
+        anim = animations[(int)direction];
+        
+        anim.Position = new Vector2(position.X - 48, position.Y - 48);
+        if (isMoving)
+        {
+            anim.Update(gameTime);
+        }
+        else
+        {
+            anim.setFrame(1);
+        }
+    }
+}
